67138cc96abc212c602ba5575abc643948fae558
alexander sampson <a.sampson206@gmail.com>
2023-09-17 14:28:31 -0700

    hello bello

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(cards, string) {
+    let newArray = []
+  for (let i = 0; i < cards.length; i++) {
+newArray.push(`Thank you, ${cards[i]}, for the wonderful ${string} gift!`);
+  }
+  return newArray;
+}
+function countDown(num) {
+while (num >= 0) {
+console.log(num--)}
+}
+
+
+
+
